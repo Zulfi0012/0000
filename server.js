5b654e04a4eb83558147e4dfa0cd3abebc0566e9
Zulfi0012 <sushichaudhary51@gmail.com>
2025-08-05 15:55:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'https://climatewise.vercel.app', 'https://hackathon-nu-lilac.vercel.app', 'http://192.168.1.8:5173'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'https://climatewise.vercel.app', 'https://nn-orcin.vercel.app','https://climateai.online','http://climateai.online' 'http://192.168.1.8:5173'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
